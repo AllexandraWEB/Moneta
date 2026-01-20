c3b1e11abf1be8a65b7afa995224d9a869315481
Aleksandra Tsimentarova <cimentarowa@gmail.com>
2026-01-20 09:30:38 +0200

    fix: Build issue

---
+++ src/app/(app)/[workspace]/page.tsx
@@ -0,0 +1,6 @@
+import HeroSection from "@/src/app/(app)/[workspace]/_components/home-section";
+import ComingSoon from "@/src/components/coming-soon";
+
+export default function WorkspacePage() {
+return <ComingSoon />
+}
